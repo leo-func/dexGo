f6e3c2ef4b8367b1ea60cef8ba3de4ad1ee45c2c
leoxzx <pbr93171@gmail.com>
2026-09-19 13:26:58 -0300

    feat: display a random pokemon every second when there's no pokemon data

--- src/viewmodel/pokedex.viewmodel.ts
+++ src/viewmodel/pokedex.viewmodel.ts
@@ -6,17 +6,30 @@ export function usePokedexViewModel() {
     const [pokemon, setPokemon] = useState<Pokemon | null> (null)
     const [pokeNameOrId, setPokeNameOrId] = useState<string | "">("")
     const [pokeId, setPokeId] = useState(1)
+    const [isRandomize, setIsRandomize] = useState(false)
     const [loading, setLoading] = useState(false)
-    const [error, setError] = useState<string | null>(null)
+    const [error, setError] = useState<Error | null>()
+    const [direction, setDirection] = useState<"left" | "right">("right");
 
     const pokeIdRef = useRef(1)
 
     useEffect(() => {
-        HandlePokemon(pokeId, null)
+        const interval = setInterval(() => {
+            if (isRandomize) {
+                const randNumber = Math.floor(Math.random() * 1026)
+                HandlePokemon(randNumber, null, isRandomize)
+            }
+        }, 1000);
+
+        return () => clearInterval(interval)
+    }, [isRandomize])
+
+    useEffect(() => {
+        HandlePokemon(pokeId, null, isRandomize)
     }, [pokeId])
 
 
-    async function HandlePokemon(id: number | null = null, name: string | null = null) {
+    async function HandlePokemon(id: number | null = null, name: string | null = null, random = false) {
         try {
             setLoading(true)
             setError(null)
@@ -25,34 +38,41 @@ export function usePokedexViewModel() {
             
             if (!data) return
 
+            if (!random) {
+                setIsRandomize(false)
+            }
+
             pokeIdRef.current = data?.id
 
             setPokeId(pokeIdRef.current)
             setPokemon(data)
             
         } catch (exception) {
-            if (exception instanceof Error){
-                setError(exception.message)
-            }
+            setError(exception as Error)
         } finally {
             setLoading(false)
         }
     }
 
-    function HandleIcrement() { 
+    function HandleIcrement() {
+        setDirection("right") 
+        setIsRandomize(false)
         setPokeId(prev => prev + 1)
     }
 
     function HandleDecrement() {
         if (pokeId <= 1) return
 
+        setDirection("left")
+        setIsRandomize(false)
         setPokeId(prev => prev - 1)
     }
 
     function HandleChange(query: string) {
-        setPokeNameOrId(query);
+        setPokeNameOrId(query)
 
         if (query.trim() === "") {
+            setIsRandomize(true)
             setPokemon(null);
             return;
         }
@@ -63,7 +83,7 @@ export function usePokedexViewModel() {
             HandlePokemon(Number(query), null); 
             setPokeId(Number(query))
         } else {
-            HandlePokemon(null, query); 
+            HandlePokemon(null, query);
         }
     }
     
@@ -73,8 +93,10 @@ export function usePokedexViewModel() {
         HandleDecrement,
         HandleChange,
         pokemon,
+        isRandomize,
         loading,
         pokeNameOrId,
         error,
+        direction,
     }
 }
